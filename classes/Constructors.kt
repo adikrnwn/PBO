6c7c309ca738c714cc37e27e4ce28ac7d84e0fbe
adikrnwn <101329970+adikrnwn@users.noreply.github.com>
2022-03-24 21:30:58 +0700

    Add files via upload

---
+++ classes/Constructors.kt
@@ -0,0 +1,12 @@
+class Car(var brand: String, var model: String, var year: Int)
+
+
+fun main() {
+    val c1 = Car("Ford", "Mustang", 1969)
+    val c2 = Car("BMW", "X5", 1999)
+    val c3 = Car("Tesla", "Model S", 2020)
+
+    println(c1.model)
+    println(c2.brand)
+    println(c3.year)
+}
